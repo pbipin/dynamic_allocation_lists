47d43e8080b525e648785551c4dc29dd3ce24480
Bipin P <bipinp2013@gmail.com>
2020-05-06 17:16:58 +0530

    Create dynamic_allocation_tuples.py

---
+++ dynamic_allocation_tuples.py
@@ -0,0 +1,38 @@
+"""
+Author: Bipin P. (mailto: bipinp2013@gmail.com)
+http://iambipin.com
+101010101    10  101010101    10  101     10    101010101
+1010101010   10  1010101010   10  1010    10    1010101010
+10      101  10  10      101  10  10 01   10    10      101
+10      101  10  10      101  10  10  10  10    10      101
+1010101010   10  1010101010   10  10   01 10    1010101010
+1010101010   10  101010101    10  10    1010    101010101
+10      101  10  10           10  10     010    10
+10      101  10  10           10  10      10    10
+1010101010   10  10           10  10      10    10
+101010101    10  10           10  10      10    10  10
+Python Program that converts a floating point decimal number to binary number
+"""
+list_main = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+folds = int(input('Enter the no. of folds: ').rstrip())
+section = int(len(list_main)/folds)
+start_index = 0
+end_index = 0
+counter = 1
+
+for i in range(folds):
+    if counter == 1:
+        end_index += section
+        globals()['tuple{0}' .format(i)] = tuple(list_main[start_index:end_index])
+        counter += 1
+    elif counter == folds:
+        globals()['tuple{0}' .format(i)] = tuple(list_main[end_index: ])
+        counter += 1
+    elif counter < folds:
+        start_index = end_index
+        end_index += section
+        globals()['tuple{0}' .format(i)] = tuple(list_main[start_index:end_index])
+        counter += 1
+
+for i in range(folds):
+    print('Tuple {0}: {1}' .format(i, globals()['tuple{0}' .format(i)]))
